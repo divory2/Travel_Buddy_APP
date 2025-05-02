e5626ab11ed63139cf904bd26302410e4dfa29a7
Devon  Ivory <devonivory@Devons-MacBook-Air.local>
2025-05-02 09:58:17 -0400

    added suggestion api request to get the name of places and then added the directions api request to get the directions to selected destiatoions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ plugins {
         
 android {
     namespace = "com.example.travel_buddy_app"
-    compileSdk = 34
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 35
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -32,7 +32,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
 val placesApiKey: String = project.findProperty("PLACES_API_KEY") as String? ?: ""
--- lib/itenerary_planner.dart
+++ lib/itenerary_planner.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:uuid/uuid.dart';
 import 'package:http/http.dart' as http;
 import '.env.dart' as placesApiKey;
@@ -20,6 +22,10 @@ final _controller =  TextEditingController();
 var uuid =  const Uuid();
 String _sessionToken = '1234567890';
 List<dynamic> _placeList = [];
+List<dynamic> _directionPlaceList = [];
+List<dynamic> legs = [];
+List<dynamic> steps =[];
+List <String> insturctionsList = [];
 List<String> destinationList=[];
 
 
@@ -48,6 +54,7 @@ void _addToDestinationList(String des){
 
 
 
+
 void getSuggestion(String input) async {
 
 
@@ -66,6 +73,13 @@ void getSuggestion(String input) async {
       setState(() {
         print('********* Response body:********${response.body}');
         _placeList = json.decode(response.body)['predictions'];
+       // _placeListId = json.decode(response.body)['place_id'];
+
+        print("PlaceList##################: $_placeList");
+       //'
+       //[['['
+       //']]] print("**************************PlaceListID##################: $_placeListId");
+
       });
     } else {
       throw Exception('Failed to load predictions');
@@ -73,6 +87,11 @@ void getSuggestion(String input) async {
   }catch(e){
     print(e);
   }
+
+
+
+
+
 
 }
 Future<void> _viewDestinations(BuildContext context) async {
@@ -86,9 +105,17 @@ Future<void> _viewDestinations(BuildContext context) async {
             ? Text('No destinations added yet.')
             : SingleChildScrollView(
                 child: ListBody(
-                  children: destinationList
-                      .map((destination) => ListTile(title: Text(destination)))
-                      .toList(),
+                  children: destinationList.map((destination){
+                        return ListTile(
+                          title: Text(destination),
+                          trailing: IconButton(
+                                  onPressed: () => _getDirectionsTo(destination, context), 
+                                  icon: Icon(Icons.directions)
+                          ),
+                          
+                          
+                        );
+                      }).toList(),
                 ),
               ),
       ),
@@ -100,6 +127,42 @@ Future<void> _viewDestinations(BuildContext context) async {
       ],
     ),
   );
+}
+
+void _ShowDirections(BuildContext context){
+    showDialog(
+    context: context,
+    builder: (_) => AlertDialog(
+      title: Text('Your Directions'),
+      content: SizedBox(
+        width: double.maxFinite,
+        child: insturctionsList.isEmpty
+            ? Text('No Directions added yet.')
+            : SingleChildScrollView(
+                child: ListBody(
+                  children: insturctionsList.map((insturction){
+                    return ListTile(
+                      title: Text(insturction),
+                    );
+                  }).toList(),
+                )
+                 
+                ),
+              ),
+              actions: [
+        TextButton(
+          onPressed: () { 
+            Navigator.pop(context);
+            setState(() {
+              insturctionsList.clear();
+            });
+            } ,
+          child: Text('Close'),
+        )
+      ],
+
+    )
+    );
 }
 
   @override
@@ -137,6 +200,12 @@ Future<void> _viewDestinations(BuildContext context) async {
                     _addToDestinationList(_placeList[index]["description"]);
                   },
                   child: ListTile(
+                    leading: IconButton(
+                      onPressed: (){
+                        _getDirectionsTo(_placeList[index]["place_id"],context);
+                      }, 
+                      icon: Icon(Icons.directions)
+                      ),
                     title: Text(_placeList[index]["description"]),
                   ),
                 );
@@ -144,11 +213,102 @@ Future<void> _viewDestinations(BuildContext context) async {
             ),),
             ElevatedButton(onPressed: (){
                 _viewDestinations(context);
-            }, child: Text('View Destination List'))
+            }, child: Text('View Destination List')),
+            
           ],
         ),
       ),
 
     );
   }
+
+ 
+
+
+
+Future<Position> _getCurrentLocation() async {
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    throw Exception('Location services are disabled.');
+  }
+
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      throw Exception('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    throw Exception('Location permissions are permanently denied');
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
+
+
+
+
+
+Future<void> _getDirectionsTo(String destinationid, BuildContext context) async {
+    String placesApiKey= PLACES_API_KEY;
+
+  try{
+
+    print("Destination Id ************* $destinationid");
+    final origindestination = await _getCurrentLocation();
+    final origin = '${origindestination.latitude},${origindestination.longitude}';
+    print("######Your Destination latitude and longitude${origin}***********************");
+    String baseURL = 'https://maps.googleapis.com/maps/api/directions/json';
+    String request = '$baseURL?destination=place_id:$destinationid&origin=$origin&key=$placesApiKey';
+    var response = await http.get(Uri.parse(request));
+    var data = json.decode(response.body);
+    if (kDebugMode) {
+      print('mydata');
+      print(data);
+    }
+    if (response.statusCode == 200) {
+      setState(() {
+        print('********* Response body:********${response.body}');
+        _directionPlaceList = json.decode(response.body)['routes'];
+        if(_directionPlaceList.isNotEmpty){
+           legs = _directionPlaceList[0]['legs'] as List<dynamic>;
+        }
+        if(legs.isNotEmpty){
+          steps = legs[0]['steps'] as List<dynamic>;
+          for(final step in steps){
+              final htmInstuction =step['html_instructions'];
+              final tagsRemvoed = removeHTMLTags(htmInstuction);
+            insturctionsList.add(tagsRemvoed);
+          }
+        }
+      
+       // _placeListId = json.decode(response.body)['place_id'];
+        print("PlaceList##################: $_directionPlaceList");
+        print("**************************PlaceListID##################: $_directionPlaceList");
+          _ShowDirections(context);
+      });
+
+    } else {
+      throw Exception('Failed to load predictions');
+    }
+  }catch(e){
+    print(e);
+  }
+
+
+
+
+}
+String removeHTMLTags(String instuctions){
+  final RegExp exp = RegExp(r'<[^>]*>', multiLine: true, caseSensitive: true);
+  return instuctions.replaceAll(exp, '');
+}
+
+
+
+
+
+
 }
